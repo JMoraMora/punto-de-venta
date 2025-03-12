2b23df58efa28671ac1c1352b02730f553ffe47d
Jaison Mora <jasonjairmoramora@gmail.com>
2025-03-12 16:57:52 -0500

    Sort products by ID in descending order on index page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index(): View
     {
-        $products = Product::paginate(12);
+        $products = Product::orderBy('id', 'desc')->paginate(12);
         return view('product.index', compact('products'));
     }
 
