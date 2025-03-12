7b72023b918a927678a8feefd2ae7abb66d8e1d6
Jaison Mora <jasonjairmoramora@gmail.com>
2025-03-11 20:26:39 -0500

    Add Sale model with fillable fields and relationships to Product and User

---
+++ app/Models/Sale.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Sale extends Model
+{
+    protected $fillable = ['code', 'customer', 'document', 'email,',  'user_id', 'product_id', 'quantity', 'total'];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
