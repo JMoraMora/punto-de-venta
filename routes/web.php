917031eed0863f5272da47fc9d11805c0675ee3b
Jaison Mora <jasonjairmoramora@gmail.com>
2025-03-11 20:15:04 -0500

    Add routes for Sale management including index, create, edit, update, and destroy

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SaleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,4 +16,13 @@ Route::controller(ProductController::class)->group(function () {
     Route::get('/products/{product}/edit', 'edit')->name('products.edit');
     Route::put('/products/{product}', 'update')->name('products.update');
     Route::delete('/products/{product}', 'destroy')->name('products.destroy');
+});
+
+Route::controller(SaleController::class)->group(function () {
+    Route::get('/sales', 'index')->name('sales.index');
+    Route::get('/sales/create', 'create')->name('sales.create');
+    Route::post('/sales', 'store')->name('sales.store');
+    Route::get('/sales/{sale}/edit', 'edit')->name('sales.edit');
+    Route::put('/sales/{sale}', 'update')->name('sales.update');
+    Route::delete('/sales/{sale}', 'destroy')->name('sales.destroy');
 });
