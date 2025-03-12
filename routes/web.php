86435b9d13672da4e068725f2d7497c7202248b2
Jaison Mora <jasonjairmoramora@gmail.com>
2025-03-12 14:34:26 -0500

    Add role-based middleware for product and sale routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SaleController;
+use App\Http\Middleware\EnsureUserHasRole;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -18,23 +19,27 @@ Route::middleware(['auth'])->group(function () {
         return view('dashboard');
     })->middleware(['auth'])->name('dashboard');
 
-    Route::controller(ProductController::class)->group(function () {
-        Route::get('/products', 'index')->name('products.index');
-        Route::get('/products/create', 'create')->name('products.create');
-        Route::post('/products', 'store')->name('products.store');
-        Route::get('/products/{product}', 'show')->name('products.show');
-        Route::get('/products/{product}/edit', 'edit')->name('products.edit');
-        Route::put('/products/{product}', 'update')->name('products.update');
-        Route::delete('/products/{product}', 'destroy')->name('products.destroy');
+    Route::middleware(EnsureUserHasRole::class . ':administrator')->group(function () {
+        Route::controller(ProductController::class)->group(function () {
+            Route::get('/products', 'index')->name('products.index');
+            Route::get('/products/create', 'create')->name('products.create');
+            Route::post('/products', 'store')->name('products.store');
+            Route::get('/products/{product}', 'show')->name('products.show');
+            Route::get('/products/{product}/edit', 'edit')->name('products.edit');
+            Route::put('/products/{product}', 'update')->name('products.update');
+            Route::delete('/products/{product}', 'destroy')->name('products.destroy');
+        });
     });
 
-    Route::controller(SaleController::class)->group(function () {
-        Route::get('/sales', 'index')->name('sales.index');
-        Route::get('/sales/create', 'create')->name('sales.create');
-        Route::post('/sales', 'store')->name('sales.store');
-        Route::get('/sales/{sale}/edit', 'edit')->name('sales.edit');
-        Route::put('/sales/{sale}', 'update')->name('sales.update');
-        Route::delete('/sales/{sale}', 'destroy')->name('sales.destroy');
+    Route::middleware(EnsureUserHasRole::class.':seller,administrator')->group(function () {
+        Route::controller(SaleController::class)->group(function () {
+            Route::get('/sales', 'index')->name('sales.index');
+            Route::get('/sales/create', 'create')->name('sales.create');
+            Route::post('/sales', 'store')->name('sales.store');
+            Route::get('/sales/{sale}/edit', 'edit')->name('sales.edit');
+            Route::put('/sales/{sale}', 'update')->name('sales.update');
+            Route::delete('/sales/{sale}', 'destroy')->name('sales.destroy');
+        });
     });
 
 });
